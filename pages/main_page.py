4a87e7467e08f6bde2d3c71ab2b8666aeea91adc
Solo800606 <solo80@inbox.ru>
2020-04-07 10:39:26 +0300

    Added navigation between pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,7 @@
 ﻿from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage): 
     def should_be_login_link(self):
--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,8 @@
 ﻿# Запуск pytest -v --tb=line --language=en test_main_page.py
 
+import pytest
 from pages.main_page import MainPage
+from .login_page import LoginPage
 
 
 def test_guest_should_see_login_link(browser):
@@ -12,3 +14,10 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_form()
     page.should_be_register_form()
 
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
